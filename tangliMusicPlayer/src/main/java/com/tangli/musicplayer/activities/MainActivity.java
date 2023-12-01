a9c563d565daa9878241cdc4dc34e986c9ee7dc1
summer_squirrel <85954091@qq.com>
2023-12-01 21:34:09 +0800

    update code

--- tangliMusicPlayer/src/main/java/com/tangli/musicplayer/activities/MainActivity.java
+++ tangliMusicPlayer/src/main/java/com/tangli/musicplayer/activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 
 
 import com.tangli.musicplayer.R;
+import com.tangli.musicplayer.databinding.ActivityMainBinding;
 import com.tangli.musicplayer.music.MusicContent;
 import com.tangli.musicplayer.view.RecyclerViewAdapter;
 
@@ -19,27 +20,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends PlayerActivity {
 
-    private View mCoverView;
-    private View mTitleView;
-    private View mTimeView;
-    private View mDurationView;
-    private View mProgressView;
-    private View mFabView;
+
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mainBinding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
-
-
-        mCoverView = findViewById(R.id.cover);
-        mTitleView = findViewById(R.id.title);
-        mTimeView = findViewById(R.id.time);
-        mDurationView = findViewById(R.id.duration);
-        mProgressView = findViewById(R.id.progress);
-        mFabView = findViewById(R.id.fab);
-
-        // Set the recycler adapter
         RecyclerView recyclerView = findViewById(R.id.tracks);
         assert recyclerView != null;
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -49,12 +37,12 @@ public class MainActivity extends PlayerActivity {
     public void onFabClick(View view) {
         //noinspection unchecked
         ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(this,
-                new Pair<>(mCoverView, ViewCompat.getTransitionName(mCoverView)),
-                new Pair<>(mTitleView, ViewCompat.getTransitionName(mTitleView)),
-                new Pair<>(mTimeView, ViewCompat.getTransitionName(mTimeView)),
-                new Pair<>(mDurationView, ViewCompat.getTransitionName(mDurationView)),
-                new Pair<>(mProgressView, ViewCompat.getTransitionName(mProgressView)),
-                new Pair<>(mFabView, ViewCompat.getTransitionName(mFabView)));
+                new Pair<>(mainBinding.cover, ViewCompat.getTransitionName(mainBinding.cover)),
+                new Pair<>(mainBinding.lotusTitle.getRoot(), ViewCompat.getTransitionName(mainBinding.lotusTitle.getRoot())),
+                new Pair<>(mainBinding.time, ViewCompat.getTransitionName(mainBinding.time)),
+                new Pair<>(mainBinding.duration, ViewCompat.getTransitionName(mainBinding.duration)),
+                new Pair<>(mainBinding.progress, ViewCompat.getTransitionName(mainBinding.progress)),
+                new Pair<>(mainBinding.fab, ViewCompat.getTransitionName(mainBinding.fab)));
         ActivityCompat.startActivity(this, new Intent(this, DetailActivity.class), options.toBundle());
     }
 
